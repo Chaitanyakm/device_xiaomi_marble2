0362bbd1c6d71c6b8a50c00fdeda308f6a39ffda
chaitanyakm <chaitanyaarora3@gmail.com>
2024-03-17 19:51:16 +0530

    marble: Update dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 rm -rf vendor/xiaomi/marble
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_marble.git vendor/xiaomi/marble
-rm -rf vendor/xiaomi/camera
-git clone --depth=1 https://gitlab.com/saikiran2001/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera
 rm -rf device/xiaomi/marble-kernel
 git clone --depth=1 https://github.com/Chaitanyakm/device_xiaomi_marble-kernel.git device/xiaomi/marble-kernel
+rm -rf packages/apps/ViPER4AndroidFX
 git clone --depth=1 https://github.com/Chaitanyakm/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 rm -rf hardware/xiaomi/aidl/vibrator
-
+git clone --depth=1 https://github.com/Chaitanyakm/device_xiaomi_miuicamera-marble.git device/xiaomi/miuicamera-marble
+git clone --depth=1 https://gitlab.com/Chaitanyakm/vendor_xiaomi_miuicamera-marble.git vendor/xiaomi/miuicamera-marble 
 
 echo "DONE"
